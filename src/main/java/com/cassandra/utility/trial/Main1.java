491c3cda215ea32624b90474c270c972662b0950
Siddharth Verma <verma.siddharth@snapdeal.com>
2016-09-01 13:19:31 +0530

    raw files commited. refactor code. design application layer. infinite loop stuck.

--- src/main/java/com/cassandra/utility/trial/Main1.java
+++ src/main/java/com/cassandra/utility/trial/Main1.java
@@ -16,16 +16,16 @@ public class Main1 {
     public static void main(String... args) throws IOException, InterruptedException {
         if(args.length == 0){
             args=new String [4];
-            args[0]=Credentials.IP;
-            args[1]=Credentials.USERNAME;
-            args[2]=Credentials.PASSWORD;
-            args[3]=Credentials.DESCRIBERING_FILE;
+            args[0]= Constants.IP;
+            args[1]= Constants.USERNAME;
+            args[2]= Constants.PASSWORD;
+            args[3]= Constants.DESCRIBERING_FILE;
         }
         Trials trials = new Trials(args[0],args[1],args[2],args[3]);
         Map<Host,Cluster> hostClusterMap = trials.getHostToCluster();
         LinkedBlockingQueue<ResultSet> queue = new LinkedBlockingQueue<>();
         int numberOfThreads = hostClusterMap.size();
-        String tableIdentifier = "cams.product_filter_mapping";
+        String tableIdentifier = Constants.KEYSPACE+"."+Constants.TABLE_NAME;
         CountDownLatch latch = new CountDownLatch(hostClusterMap.size());
         for(Host host : hostClusterMap.keySet()){
             new Producer(hostClusterMap.get(host),trials.getHostToTokenRange().get(host.getAddress().toString().substring(1)),tableIdentifier,queue,latch);
